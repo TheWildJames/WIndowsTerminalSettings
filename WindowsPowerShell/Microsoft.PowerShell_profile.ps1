72b9ea4a81fb081bad2913f9ce4d6442db67200c
James McConnell <bins4us@hotmail.com>
2022-05-30 03:59:14 -0400

    Add files via upload

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH\clean-detailed.omp.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
